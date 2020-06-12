c5b3e9c3a63a76f59f4768358126332d1cdfc698
Arseny <arseny@i-am.studio>
2020-06-12 15:40:25 +0300

    Initial commit

---
+++ src/Heading.vue
@@ -0,0 +1,21 @@
+<template>
+    <component
+        v-bind:is="`h${level}`">
+        {{ text }}</component>
+</template>
+
+<script>
+export default {
+    props: {
+        level: {
+            type: Number,
+            required: true
+        },
+        text: String
+    }
+}
+</script>
+
+<style>
+
+</style>
