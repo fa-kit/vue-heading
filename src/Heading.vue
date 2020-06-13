e0e790065475615d42efe986fa501a0fb2835f24
Arseny <arseny@i-am.studio>
2020-06-13 18:20:21 +0300

    Fixed doc

--- src/Heading.vue
+++ src/Heading.vue
@@ -16,7 +16,7 @@ export default {
      * @property {number} level         — For correct heading level (h[level]).
      * @property {float} lineHeight     — Non important value, have fallback from original styles.
      * @property {boolean} calcOffset   — Trigger to start calculation compensation for test lines.
-     * @propery {array} calcMove        — Contain ratio that will be used in calculating additional offset.
+     * @property {array} calcMove        — Contain ratio that will be used in calculating additional offset.
      */
     name: 'Heading',
     props: {
